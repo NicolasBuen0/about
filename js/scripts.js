ba18bf6b8ad761addcd2864c6b6d7b51c6703894
Nicolas Bueno <nickibueno@gmail.com>
2022-06-07 20:46:08 -0300

    links

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//menu
 const btn = document.querySelector("#btn-mobile");
 const nav = document.querySelector("nav");
 
@@ -7,3 +8,18 @@ function toggle() {
 
 
 btn.addEventListener("click", toggle);
+
+
+//timeout para links projeto
+
+/* const imgProjetos = document.querySelectorAll(".img-projetos");
+imgProjetos.forEach(img);
+
+function img(img) {
+    img.addEventListener("click", clicou)
+}
+
+function clicou(e) {
+    const hover = e.target.lastElementChild;
+    hover.classList.add("ativo")
+} */
