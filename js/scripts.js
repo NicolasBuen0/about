61ed845ab738f722511b61e93e33d76526407358
Nicolas Bueno <nickibueno@gmail.com>
2022-06-08 10:55:56 -0300

    scroll suave

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,35 @@ function clicou(e) {
     hover.classList.add("ativo")
 } */
 
+//animacao
 new SimpleAnime();
+
+//scroll suave
+function scrollSuave() {
+    const links = document.querySelectorAll("a[href^='#']")
+    links.forEach(link => {
+        ["touchstart", "click"].forEach(evento => {
+            link.addEventListener(evento, scrollLink)
+        })
+    })
+
+    function scrollLink(event) {
+        event.preventDefault();
+        const to = getScrollTopByHref(event.target) - 95;
+        scrollToPosition(to);
+    }
+
+    function scrollToPosition(to) {
+        window.scroll({
+            top: to,
+            behavior: "smooth",
+        });
+    }
+
+    function getScrollTopByHref(element) {
+        const id = element.getAttribute("href");
+        return document.querySelector(id).offsetTop;
+    }
+}
+
+scrollSuave()
